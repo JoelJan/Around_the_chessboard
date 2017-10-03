0fe849240ab318795fd0d571c65e2d98e87478d7
Joeladmin <joel.jancarik@gmail.com>
2017-10-03 19:22:13 +0200

    First good version

--- Main.py
+++ Main.py
@@ -4,6 +4,8 @@ import sys
 import random
 from pygame.locals import *
 
+white=(255,255,255)
+black=(0,0,0)
 class chessboard:
     def __init__(self,width,height,DISPLAY):
         self.width=width;
@@ -19,15 +21,13 @@ class chessboard:
         x=int(random.random()*8//1)+2
         y=int(random.random()*8//1)+2
         self.board[x,y]=1
-        self.basicfont = pygame.font.SysFont('Ariel', 148)
+        self.basicfont = pygame.font.SysFont('Ariel', 28)
         print x-1
         print y-1       
         self.knight=pygame.transform.scale(pygame.image.load('knight.gif'),(60,60))
-        self.move_knight(x,y)
+        self.move_knight(x,y,(0,0)) ##point (0,0) is not on screen and so wont be printed
 
     def draw(self):
-        black=(0,0,0)
-        white=(255,255,255)
         for i in range (0,self.height):
             for j in range (0,self.width):
                 if ((i+j)%2==1):
@@ -41,19 +41,27 @@ class chessboard:
         print y-1
         if self.board[x,y]==0:
             print 'empty'
-            if self.actual in [self.board[x+j[0],y+j[1]] for j in self.jump]:
-                self.move_knight(x,y)
+            for m in [(x+j[0],y+j[1]) for j in self.jump]:
+                if self.board[m]==self.actual:
+                    self.move_knight(x,y,m)
 
-    def move_knight(self,x,y):
+    def move_knight(self,x,y,last):
+        ##move_knight and add actual state
         self.actual=self.actual+1
         self.board[x,y]=self.actual
         self.DISPLAY.blit(self.knight,(60*(x-2),60*(y-2)))
-        pygame.display.flip()
-        text = self.basicfont.render(str(self.actual), True, (255, 0, 0), (255, 0, 255))
+        #removes last knight
+        if ((last[0]+last[1])%2==1):
+            pygame.draw.rect(self.DISPLAY,black,(60*(last[0]-2),60*(last[1]-2),60,60))
+        else:
+            pygame.draw.rect(self.DISPLAY,white,(60*(last[0]-2),60*(last[1]-2),60,60))
+        ##add number on the square
+        text = self.basicfont.render(str(self.actual-2), True, (255, 0, 0), (100, 100, 100))
         textrect = text.get_rect()
-        textrect.centerx = x*60-5
-        textrect.centery = y*60-5
+        textrect.centerx = last[0]*60-90
+        textrect.centery = last[1]*60-90
         self.DISPLAY.blit(text, textrect)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     pygame.init()
